8b5ad488047c305a8af65a977b9f099daa3c6a6a
Kevin Nderitu <nderitukelvin19@gmail.com>
2016-08-16 23:32:34 +0300

    Dissabled SQLALCEMY_TRACK_MODIFICATIONS

--- config.py
+++ config.py
@@ -5,5 +5,6 @@ import sys, os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:@localhost:3306/pastpapers";
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:@localhost:3306/pastpapers"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
