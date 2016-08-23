0f12dc357ec6d3e34d3eae7c5a1556095ffd8a6f
Kevin Nderitu <nderitukelvin19@gmail.com>
2016-08-23 23:45:42 +0300

    Config DB
    Added db string from settings

--- config.py
+++ config.py
@@ -1,11 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from settings import *
 import sys, os
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:@localhost:3306/pastpapers"
+app.config["SQLALCHEMY_DATABASE_URI"] = DevelopmentConfig.DATABASE_URI
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.secret_key = "thisisthesecretkeypleasedonotread"
 db = SQLAlchemy(app)
