25fd2cd73a11900fdef0c4af1b1d6007c8c65dd7
Kevin Nderitu <nderitukelvin19@gmail.com>
2016-08-23 23:44:45 +0300

    Utilities
    Added new utility methods

--- utilities/utilities.py
+++ utilities/utilities.py
@@ -1,6 +1,8 @@
 import uuid
 import hashlib
 import random
+import boto3
+from pastpapers.settings import *
 
 class Utilities:
 
@@ -19,3 +21,16 @@ class Utilities:
     def magic_word_random_flash():
         flashes = ["Are you sure that's the magic word?", "Now, relax and try again", "Try again, Am still here. Waiting.."]
         return flashes[random.randint(0, len(flashes) - 1)]
+
+    @staticmethod
+    def allowed_file(filename):
+        ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'docx'])
+        return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+    @staticmethod
+    def threaded_upload(filename, file):
+        s3 = boto3.resource("s3")
+        bucket = s3.Bucket(DevelopmentConfig.S3_BUCKET_NAME)
+        file_contents = file.read()
+        bucket.put_object(Key = filename, Body = file_contents)
+        return filename
