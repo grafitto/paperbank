d67753e082272ba3431cbbaac0fcad23b70f2d44
Kevin Nderitu <nderitukelvin19@gmail.com>
2016-09-03 18:58:34 +0300

    Search
    Implimented Paper search

--- static/js/client/controllers.js
+++ static/js/client/controllers.js
@@ -4,24 +4,55 @@ app.controller("MainController", ["$scope", "$rootScope", "Server", function ($s
     $scope.departments = $rootScope.departments;
     $scope.course = {};
     $scope.unitsEmpty = false;
-    let cache = {};
+    $scope.breadCrumb = [];
+    $scope.loading = false;
+    $scope.isSearch = false;
+    $scope.papers = [];
 
-    $scope.getUnits = function (courseId) {
-        if (cache[courseId] != undefined) {
-            $scope.course = cache[courseId];
-            $scope.unitsEmpty = $scope.course.unit.length == 0;
-            console.log("Got from cache...");
-        } else {
-            Server.course(courseId)
-            .then(function (resp) {
-                $scope.course = resp;
-                cache[courseId] = resp;
-                $scope.unitsEmpty = $scope.course.unit.length == 0;
-                console.log("Fetched then cached...");
-            }, function (err) {
+    $scope.search = function(){
+        let q = $("#q").val();
+        q = "%" + q + "%";
+        //let query = {"filters":[{"or":[{"name":"title","op":"like","val": q},{"name":"lecturer","op":"like","val":q}]}]}
+        let query = {filters:[{name:"title",op:"like",val:q}]};
+        if(q == "%%"){
+            $scope.isSearch = false;
+        }else{
+            $scope.isSearch = true;
+        }
+        $scope.loading = true;
+        Server.searchPaper(query)
+            .then(function(resp){
+                $scope.loading = false;
+                $scope.papers = resp.objects;
+            },
+            function(err){
                 console.log(err);
             })
-        }
-        //
     }
+    $scope.getUnits = function (courseId) {
+            $scope.loading = true;
+            Server.course(courseId)
+                .then(function (resp) {
+                    $scope.course = resp;
+                    //Populate preadcrump
+                    if($scope.breadCrumb.length > 1){
+                        let first = $scope.breadCrumb.shift();
+                        while($scope.breadCrumb.length){ $scope.breadCrumb.pop()}
+                        $scope.breadCrumb.push(first);
+                    }
+                    $scope.breadCrumb.push(resp.title);
+                    $scope.loading = false;
+                }, function (err) {
+                    console.log(err);
+                })
+        }
+
+    $scope.populateBreadCrumb = function(text){
+        while($scope.breadCrumb.length){ $scope.breadCrumb.pop()}
+        $scope.breadCrumb.push(text);
+    }
+
+    $scope.preventDefaultA = function(){ return true;}
 }]);
+
+
