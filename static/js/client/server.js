e7249d34475946febf3971ffc7d7feaaa57c47f4
Kevin Nderitu <nderitukelvin19@gmail.com>
2016-09-03 18:59:09 +0300

    Angular Server
    Implimented the needed methods in the client's angular

--- static/js/client/server.js
+++ static/js/client/server.js
@@ -29,11 +29,41 @@ app.factory("Server", ["$http", "$q", function ($http, $q) {
         let deferred = $q.defer();
         $http.get(baseUrl + "/course/" + courseId)
             .success(function (resp) {
+                resp.unit.forEach( function(unit, index) {
+                    $http.get(baseUrl + "/unit/" + unit.id)
+                        .success(function(resp){
+                            unit["paper"] = resp.paper;
+                        })
+                });
+                console.log(resp);
                 deferred.resolve(resp);
             })
             .error(function (err) {
                 deferred.reject(err);
             });
+        return deferred.promise;
+    }
+    server.departmentsPopulate = function(){
+        let deferred = $q.defer();
+        $http.get(baseUrl + "/department")
+            .success(function(resp){
+                deferred.resolve(resp);
+            })
+            .error(function(err){
+                deferred.reject(err);
+            })
+
+        return deferred.promise;
+    }
+    server.searchPaper = function(query){
+        let deferred = $q.defer();
+        $http.get(baseUrl + "/paper?q=" + JSON.stringify(query))
+            .success(function(resp){
+                deferred.resolve(resp);
+            })
+            .error(function(err){
+                deferred.reject(err);
+            })
         return deferred.promise;
     }
     return server;
