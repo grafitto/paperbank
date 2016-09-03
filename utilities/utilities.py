60b1592f43a881de20ffdbf14534324e4310871f
Kevin Nderitu <nderitukelvin19@gmail.com>
2016-09-03 20:00:49 +0300

    Removed the threaded upload

--- utilities/utilities.py
+++ utilities/utilities.py
@@ -1,8 +1,7 @@
 import uuid
 import hashlib
 import random
-import boto3
-from pastpapers.settings import *
+import boto3 
 
 class Utilities:
 
@@ -26,11 +25,3 @@ class Utilities:
     def allowed_file(filename):
         ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'docx'])
         return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
-
-    @staticmethod
-    def threaded_upload(filename, file):
-        s3 = boto3.resource("s3")
-        bucket = s3.Bucket(DevelopmentConfig.S3_BUCKET_NAME)
-        file_contents = file.read()
-        bucket.put_object(Key = filename, Body = file_contents)
-        return filename
