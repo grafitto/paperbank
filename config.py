95f554350dfade90795e57277c518f1b2382467f
Kevin Nderitu <nderitukelvin19@gmail.com>
2016-09-03 19:32:33 +0300

    Added the production database URL

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ import sys, os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = DevelopmentConfig.DATABASE_URI
+app.config["SQLALCHEMY_DATABASE_URI"] = ProductionConfig.DATABASE_URI
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.secret_key = "thisisthesecretkeypleasedonotread"
 db = SQLAlchemy(app)
--- settings.py
+++ settings.py
@@ -4,7 +4,7 @@ class Config(object):
     DATABASE_URI = 'sqlite://:memory:'
 
 class ProductionConfig(Config):
-    DATABASE_URI = 'mysql://user@localhost/foo'
+    DATABASE_URI = 'mysql+pymysql://be1a28832dc3d6:a4fda910@us-cdbr-iron-east-04.cleardb.net/heroku_0adc8d16ff6feca?reconnect=true'
     S3_BUCKET_NAME = "anu.pastpapers"
     AWS_ACCESS_KEY_ID = 'AKIAJVEPXVGPPURXOR3Q'
     AWS_SECRET_ACCESS_KEY = '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'
