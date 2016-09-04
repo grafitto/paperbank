bd1e37a8f379028b0b8170ffc8030abac2308b7b
Kevin Nderitu <nderitukelvin19@gmail.com>
2016-09-04 20:43:33 +0300

    Changed port to 5000

--- index.py
+++ index.py
@@ -26,6 +26,6 @@ manager.create_api(Unit, methods = ["GET"])
 #Fire that thing
 if __name__ == "__main__":
     from os import environ
-    arg_port = int(environ.get('PORT', 80))
+    arg_port = int(environ.get('PORT', 5000))
     print(arg_port)
     admin.app.run(host='0.0.0.0', port= arg_port, debug = False)
