6a8c6d1618d865a120f95aff4dc650ea836d09f0
Kevin Nderitu <nderitukelvin19@gmail.com>
2016-09-04 19:16:05 +0300

    Testing

--- index.py
+++ index.py
@@ -1,23 +1,22 @@
-from config import app
-from config import db
+
 from models import * 
 import flask_restless as fl
 
 import admin
 
-db.create_all()
+admin.db.create_all()
 
 #This serves all angular partials
-@app.route('/partials/<path:path>')
+@admin.app.route('/partials/<path:path>')
 def serve_partial(path):
     return admin.render_template('/client/partials/{}'.format(path))
 
 #This kickstarts the client app
-@app.route('/')
+@admin.app.route('/')
 def index():
     return admin.render_template("client/index.html")
 
-manager = fl.APIManager(app, flask_sqlalchemy_db = db)
+manager = fl.APIManager(admin.app, flask_sqlalchemy_db = admin.db)
 
 manager.create_api(Paper, methods = ["GET"])
 manager.create_api(Course, methods = ["GET"])
@@ -29,4 +28,4 @@ if __name__ == "__main__":
     from os import environ
     arg_port = int(environ.get('PORT', 5000))
     print(arg_port)
-    app.run(host='0.0.0.0', port= arg_port, debug = False)
+    admin.app.run(host='0.0.0.0', port= arg_port, debug = False)
