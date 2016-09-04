47db3204f11987e630c05cd535327d1d3abe3126
Kevin Nderitu <nderitukelvin19@gmail.com>
2016-09-04 18:41:04 +0300

    Struggling

--- index.py
+++ index.py
@@ -28,4 +28,5 @@ manager.create_api(Unit, methods = ["GET"])
 if __name__ == "__main__":
     from os import environ
     arg_port = int(environ.get('PORT', 5000))
+    print(arg_port)
     app.run(host='0.0.0.0', port= arg_port, debug = True)
