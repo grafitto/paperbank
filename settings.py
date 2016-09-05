0d74c948e15bbbba0e40d9bf4a0f5a21ef48c535
Kevin Nderitu <nderitukelvin19@gmail.com>
2016-09-05 22:51:00 +0300

    Removed access ids

--- settings.py
+++ settings.py
@@ -6,15 +6,11 @@ class Config(object):
 class ProductionConfig(Config):
     DATABASE_URI = 'mysql+pymysql://grafitto:1234567youtube@anu-paperbank.cuczdri4rcvf.us-west-2.rds.amazonaws.com:3306/gsayyicxj49vmbfo'
     S3_BUCKET_NAME = "anu.pastpapers"
-    AWS_ACCESS_KEY_ID = 'AKIAJVEPXVGPPURXOR3Q'
-    AWS_SECRET_ACCESS_KEY = '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'
 
 class DevelopmentConfig(Config):
     DEBUG = True
     S3_BUCKET_NAME = "anu.pastpapers"
     DATABASE_URI = "mysql+pymysql://root:@localhost:3306/pastpapers"
-    AWS_ACCESS_KEY_ID = 'AKIAJVEPXVGPPURXOR3Q'
-    AWS_SECRET_ACCESS_KEY = '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'
 
 class TestingConfig(Config):
     TESTING = True
