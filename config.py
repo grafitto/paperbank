0786447fb9c877177f2cfb5c42f74415858c0f74
Kevin Nderitu <nderitukelvin19@gmail.com>
2016-10-21 01:18:28 +0300

    Changed to Development mode
    Switched to development mode to enable debuging and local database

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from settings import *
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = ProductionConfig.DATABASE_URI
+app.config["SQLALCHEMY_DATABASE_URI"] = DevelopmentConfig.DATABASE_URI
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.secret_key = "thisisthesecretkeypleasedonotread"
 db = SQLAlchemy(app)
