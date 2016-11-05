3b5fcf186e342e5d519d675462dc8604317bfc6b
Kevin Nderitu <nderitukelvin19@gmail.com>
2016-11-06 00:27:02 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,16 +1,1 @@
-class Config(object):
-    DEBUG = False
-    TESTING = False
-    DATABASE_URI = 'sqlite://:memory:'
-
-class ProductionConfig(Config):
-    DATABASE_URI = 'mysql+pymysql://grafitto:1234567youtube@anu-paperbank.cuczdri4rcvf.us-west-2.rds.amazonaws.com:3306/gsayyicxj49vmbfo'
-    S3_BUCKET_NAME = "anu.pastpapers"
-
-class DevelopmentConfig(Config):
-    DEBUG = True
-    S3_BUCKET_NAME = "anu.pastpapers"
-    DATABASE_URI = "mysql+pymysql://root:@localhost:3306/pastpapers"
-
-class TestingConfig(Config):
-    TESTING = True
+#This is where all the settings reside
